9f090228b2f55841f895abb26ee90d6a8b66066b
Victor-Salomon <victor-salomon@hotmail.fr>
2023-11-23 16:56:33 +0100

    refacto connection + init ticket page

--- components/Navigation/Header/Header.tsx
+++ components/Navigation/Header/Header.tsx
@@ -1,36 +1,8 @@
-"use client";
 import Link from "next/link";
 import TernoaIcon from "@/assets/providers/Ternoa";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { ethers } from "ethers";
-import { useUserWalletStore } from "@/stores/walletStore";
-import { middleEllipsis } from "@/lib/utils";
+import UserConnection from "../UserConnection";
 
 const Header = () => {
-  const user = useUserWalletStore((state) => state.userWallet);
-  const setUser = useUserWalletStore((state) => state.setUserWallet);
-  const connectToMetaMask = async () => {
-    const ethereum = (window as any).ethereum;
-    if (typeof ethereum !== "undefined") {
-      try {
-        const provider = new ethers.BrowserProvider(ethereum);
-        const signer = await provider.getSigner();
-        setUser({ address: signer.address, isConnected: true });
-
-        ethereum.on("accountsChanged", async function () {
-          const provider = new ethers.BrowserProvider(ethereum);
-          const signer = await provider.getSigner();
-          setUser({ address: signer.address, isConnected: true });
-        });
-      } catch (error: Error | any) {
-        alert(`Error connecting to MetaMask: ${error?.message ?? error}`);
-      }
-    } else {
-      alert("MetaMask not installed");
-    }
-  };
-
   return (
     <nav className="container fixed top-0 left-0 right-0 backdrop-blur-md z-50">
       <div className="flex justify-between items-center py-4">
@@ -43,17 +15,7 @@ const Header = () => {
             </div>
           </div>
         </Link>
-        <Button variant={"outline"} onClick={() => connectToMetaMask()}>
-          <Image
-            src="/metamask.svg"
-            alt="MetaMask Logo"
-            className="dark:invert pe-1"
-            width={30}
-            height={30}
-            priority
-          />
-          {user.address ? middleEllipsis(user.address) : "Connect"}
-        </Button>
+        <UserConnection />
       </div>
     </nav>
   );
--- components/Navigation/UserConnection/UserConnection.tsx
+++ components/Navigation/UserConnection/UserConnection.tsx
@@ -1,5 +1,123 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { ethers } from "ethers";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn, middleEllipsis } from "@/lib/utils";
+import { useUserWalletStore } from "@/stores/walletStore";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import Link from "next/link";
+
 const UserConnection = () => {
-return <div>hello</div>    
+  const user = useUserWalletStore((state) => state.userWallet);
+  const setUser = useUserWalletStore((state) => state.setUserWallet);
+  const resetUser = useUserWalletStore((state) => state.resetUserWallet);
+  const [open, setOpen] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [showErroDialog, setShowErrorDialog] = useState<boolean>(false);
+
+  const connectToMetaMask = async () => {
+    const ethereum = (window as any).ethereum;
+    if (typeof ethereum !== "undefined") {
+      try {
+        const provider = new ethers.BrowserProvider(ethereum);
+        const signer = await provider.getSigner();
+        setUser({ address: signer.address, isConnected: true });
+
+        ethereum.on("accountsChanged", async function () {
+          const provider = new ethers.BrowserProvider(ethereum);
+          const signer = await provider.getSigner();
+          setUser({ address: signer.address, isConnected: true });
+        });
+      } catch (error: Error | any) {
+        const errDesc = `Error connecting to MetaMask: ${
+          error?.message ?? error
+        }`;
+        alert(errDesc);
+        setError(errDesc);
+        setShowErrorDialog(true);
+      }
+    } else {
+      const errDesc = "MetaMask not installed";
+      alert(errDesc);
+      setError(errDesc);
+      setShowErrorDialog(true);
+    }
+  };
+
+  return !user.isConnected ? (
+    <>
+      {/* <Dialog open={showErroDialog} onOpenChange={setShowErrorDialog}>
+    f <DialogTrigger asChild> */}
+      <Button variant={"outline"} onClick={() => connectToMetaMask()}>
+        <Image
+          src="/metamask.svg"
+          alt="MetaMask Logo"
+          className="dark:invert pe-1"
+          width={30}
+          height={30}
+          priority
+        />
+        Connect
+      </Button>
+      {/* </DialogTrigger>
+       <DialogContent className="sm:max-w-[425px] px-2 sm:px-6">
+         <div>
+           <div className="py-4 w-72 m-2 rounded-md bg-gradient-to-r from-pink-900 via-fuchsia-900 to-red-900">
+             <div className="text-sm text-center font-light bg-gradient-to-r from-red-300 to-pink-600 bg-clip-text text-transparent mx-2">
+               {error}
+             </div>
+           </div>
+         </div>
+       </DialogContent>
+     </Dialog> */}
+    </>
+  ) : (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant="outline" role="combobox" aria-expanded={open}>
+          <Image
+            src="/metamask.svg"
+            alt="MetaMask Logo"
+            className="dark:invert pe-1"
+            width={30}
+            height={30}
+            priority
+          />
+          <span className="from-purple-500 via-pink-500 to-blue-500 bg-gradient-to-r bg-clip-text text-transparent font-light">
+            {middleEllipsis(user.address, 15)}
+          </span>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0 w-max" align="end">
+        <div className="flex flex-col px-5 py-3 space-y-1">
+          <div className={cn(buttonVariants({ variant: "outline" }), "cursor-pointer")}>Create an Event</div>
+          <Link
+            href="/tickets"
+            className={buttonVariants({ variant: "link" })}
+          >
+            My tickets
+          </Link>
+        </div>
+        <div className="border-t text-end">
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="text-xs text-muted-foreground font-light m-2"
+            onClick={() => resetUser()}
+          >
+            Logout
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default UserConnection;
